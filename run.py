93c4a65b90d261f499c2c87bf2ec671bdba272f6
Evin Owen <eowen@launchcg.com>
2019-08-13 07:58:40 -0700

    Enabled tests in run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import unittest
 
-# from tests.AttachVirtualDisks import AttachVirtualDisks
 from tests.CreateVirtualMachine import CreateVirtualMachine
-# from tests.DeleteTemplate import DeleteTemplate
-# from tests.DownloadTemplate import DownloadTemplate
-# from tests.StartTemplate import StartTemplate
-# from tests.StopTemplate import StopTemplate
+from tests.StartVirtualMachine import StartVirtualMachine
+from tests.AttachVirtualDisks import AttachVirtualDisks
+from tests.DownloadTemplate import DownloadTemplate
+from tests.StopVirtualMachine import StopVirtualMachine
+from tests.DeleteVirtualMachine import DeleteVirtualMachine
 
 if __name__ == '__main__':
     unittest.main()
